2e5782eed41f469733c1c8c4c5ddd940993f3d74
Tamik <hello@Tamik.ru>
2020-09-26 10:55:52 +0300

    Object stringifier

---
+++ src/stringifiers/object/index.ts
@@ -0,0 +1,23 @@
+import type {IStringifier} from '../../Stringifier';
+import {stringify} from '../../utils';
+
+function stringifyObject(this: any, stringifier: IStringifier): void {
+    const keys = Object.keys(this).sort();
+    let key: string;
+
+    stringifier.string += '{';
+
+    for (let i = 0; i < keys.length; i++) {
+        key = keys[i];
+
+        stringifier.string += key;
+        stringifier.update(this[key]);
+    }
+
+    stringifier.string += '}';
+}
+
+// @ts-ignore
+Object.prototype[stringify] = stringifyObject;
+
+export default stringifyObject;
