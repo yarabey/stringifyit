8266b845008f9a5ea8c27516921d2fa0c3e6cc6e
Tamik <hello@Tamik.ru>
2020-09-26 10:55:39 +0300

    Array stringifier

---
+++ src/stringifiers/array/index.ts
@@ -0,0 +1,47 @@
+import {IStringifier, stringifyit} from '../../Stringifier';
+import {stringify} from '../../utils';
+
+function stringifyArray(this: Array<any>, stringifier: IStringifier, forceSort?: boolean): void {
+    let index = this.length;
+
+    stringifier.string += '[';
+
+    if (forceSort || stringifier.options.sortArrays) {
+        const arrayOfStrings: string[] = [];
+
+        while (index-- > 0) {
+            arrayOfStrings.push(stringifyit(this[index], stringifier.options));
+        }
+
+        stringifier.string += arrayOfStrings.sort().join('');
+    } else {
+        while (index-- > 0) {
+            stringifier.update(this[index]);
+        }
+    }
+
+    stringifier.string += ']';
+}
+
+// @ts-ignore
+Array.prototype[stringify] = stringifyArray;
+// @ts-ignore
+Int8Array.prototype[stringify] = stringifyArray;
+// @ts-ignore
+Uint8Array.prototype[stringify] = stringifyArray;
+// @ts-ignore
+Uint8ClampedArray.prototype[stringify] = stringifyArray;
+// @ts-ignore
+Int16Array.prototype[stringify] = stringifyArray;
+// @ts-ignore
+Uint16Array.prototype[stringify] = stringifyArray;
+// @ts-ignore
+Int32Array.prototype[stringify] = stringifyArray;
+// @ts-ignore
+Uint32Array.prototype[stringify] = stringifyArray;
+// @ts-ignore
+Float32Array.prototype[stringify] = stringifyArray;
+// @ts-ignore
+Float64Array.prototype[stringify] = stringifyArray;
+
+export default stringifyArray;
