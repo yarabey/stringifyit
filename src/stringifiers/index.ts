1f5949bac0d6486a5695c27c8a3b0ff2fec93188
Tamik <hello@Tamik.ru>
2020-09-26 10:57:03 +0300

    Stringifier functions

---
+++ src/stringifiers/index.ts
@@ -0,0 +1,5 @@
+export {default as array} from './array';
+export {default as object} from './object';
+export {default as date} from './date';
+export {default as map} from './map';
+export {default as set} from './set';
