f2cb5ec45636702f851442b9b35489eba0ad8337
Tamik <hello@Tamik.ru>
2020-09-27 18:49:55 +0300

    Updated stringify symbol location

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
 export {default as callObjectStringify} from './callObjectStringify';
 export {default as callStringify} from './callStringify';
 export {default as handleError} from './handleError';
-export {default as stringify} from './stringifySymbol';
+export {default as stringify} from './stringify';
